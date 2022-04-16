42c903fc8e9023aeb40c4c17422c2c7dc4c08e0b
jessie <1530693531@qq.com>
2022-04-16 15:18:00 +0800

    完成个人中心的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,5 +1,5 @@
 $.ajaxPrefilter(function (options) {
-    console.log(options.url) // 获取出请求的地址
+    // console.log(options.url) // 获取出请求的地址
     //发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
     // console.log(options.url);
@@ -11,7 +11,7 @@ $.ajaxPrefilter(function (options) {
     }
     //不论失败还是成功，最终都会调用 complete 回调函数
     options.complete = function (res) {
-        console.log(res);
+        // console.log(res);
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             // 1. 强制清空 token
